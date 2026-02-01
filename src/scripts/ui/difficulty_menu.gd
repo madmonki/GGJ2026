644b44774cbc41741d07a79bae6f7cb46f3522de
Yarkin D. Cetin <yarkindc@gmail.com>
2026-02-01 23:40:42 +0300

    Tutorial scene

--- src/scripts/ui/difficulty_menu.gd
+++ src/scripts/ui/difficulty_menu.gd
@@ -4,6 +4,8 @@ extends Control
 @onready var fast_btn = $CenterContainer/VBoxContainer/Fast
 @onready var faster_btn = $CenterContainer/VBoxContainer/Faster
 @onready var extreme_btn = $CenterContainer/VBoxContainer/Extreme
+# Newly added button
+@onready var tutorial_btn = $CenterContainer/VBoxContainer/Tutorial
 
 func _ready():
 	# Vibrant visuals initialization
@@ -14,9 +16,17 @@ func _ready():
 	faster_btn.pressed.connect(_on_difficulty_selected.bind(10.0))
 	extreme_btn.pressed.connect(_on_difficulty_selected.bind(7.0))
 	
-	# Focus the regular button by default for controller/keyboard support
+	# Check if tutorial button exists (it should, but safety first if scene not reloaded)
+	if tutorial_btn:
+		tutorial_btn.pressed.connect(_on_tutorial_pressed)
+	
+	# Focus the regular button by default
 	regular_btn.grab_focus()
 
 func _on_difficulty_selected(duration: float):
 	GameEvents.selected_doom_duration = duration
 	get_tree().change_scene_to_file("res://levels/main.tscn")
+
+func _on_tutorial_pressed():
+	# Tutorial sets its own duration in its script, but we can set a default here too
+	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
